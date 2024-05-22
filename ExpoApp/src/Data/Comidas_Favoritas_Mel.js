5c8d0bdf1988cd540e2a72ffbf7878b6f7e550a3
AdrianaP-MM <20190015@ricaldone.edu.sv>
2024-05-21 20:48:34 -0600

    Pantalla 2: 100%

--- ExpoApp/src/Data/Comidas_Favoritas_Mel.js
+++ ExpoApp/src/Data/Comidas_Favoritas_Mel.js
@@ -1,21 +1,39 @@
 const Data = [
     {
         id: '1',
-        src: require('../Imagenes/pupusas-comida.jpg'),
+        src: require('../Imagenes/Hamburguesa.jpg'),
         nom: 'Hamburguesa de pollo',
-        desc: 'Son pupusas',
+        desc: 'Las hamburguesas de pollo son sándwiches con pechuga o carne molida de pollo, servidas en un panecillo con lechuga, tomate, queso y salsas. Son una opción más ligera que las de res.',
     },
     {
         id: '2',
-        src: require('../Imagenes/pupusas-comida.jpg'),
+        src: require('../Imagenes/TacosPastor.jpg'),
         nom: 'Tacos al pastor',
-        desc: 'Son pupusas',
+        desc: 'Los tacos al pastor son un platillo mexicano que consiste en tortillas de maíz rellenas de carne de cerdo marinada y asada en un trompo vertical, acompañadas de piña, cebolla y cilantro.',
     },
     {
         id: '3',
-        src: require('../Imagenes/pupusas-comida.jpg'),
+        src: require('../Imagenes/Carne.jpg'),
         nom: 'Carne Asada',
-        desc: 'Son pupusas',
+        desc: 'La carne asada es un plato que consiste en carne de res, usualmente filete o costilla, marinada y asada a la parrilla. Es popular en muchos países y se sirve con acompañamientos como guacamole, frijoles y tortillas.',
+    },
+    {
+        id: '4',
+        src: require('../Imagenes/Sushi.jpg'),
+        nom: 'Sushi',
+        desc: 'El sushi es un plato japonés de arroz y pescado fresco envuelto en alga nori. Es sabroso, fresco y se sirve con salsa de soja, wasabi y jengibre encurtido.',
+    },
+    {
+        id: '5',
+        src: require('../Imagenes/Pupusa.jpg'),
+        nom: 'Pupusas de pollo',
+        desc: 'Las pupusas de pollo son un platillo salvadoreño que consiste en tortillas gruesas de maíz rellenas de pollo desmenuzado, queso y a veces frijoles. Se sirven con curtido (repollo encurtido) y salsa de tomate.',
+    },
+    {
+        id: '6',
+        src: require('../Imagenes/Sandwich.jpg'),
+        nom: 'Sandwiches de huevo',
+        desc: 'El sándwich de huevo es un sándwich simple hecho con huevo cocido, que puede ser frito, revuelto o en forma de ensalada de huevo, servido entre dos rebanadas de pan. Es popular como desayuno o almuerzo.',
     }
 ];
 
